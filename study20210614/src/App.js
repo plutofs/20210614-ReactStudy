80b64f8393adf32d3c28e8a3b18c35635107ba5c
LeeJungHoon <thfndjan@gmail.com>
2021-06-14 17:21:38 +0900

    2

--- study20210614/src/App.js
+++ study20210614/src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
 import './App.css';
+import Potato from './Potato';
+
+function Food() {
+  return <h1>I hate dem</h1>;
+}
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>hello</div>
+      <Potato />
+      <Food name = "kimchi" />
     </div>
   );
 }
